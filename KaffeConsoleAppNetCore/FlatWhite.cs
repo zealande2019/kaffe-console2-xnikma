04560c65e2958409afb3520a48196927cb758ced
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-01 13:53:47 +0100

    Tilføjet flatwhite class Tilføjet instanser og typecast i main

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,33 @@
+﻿using kaffe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaffeConsoleAppNetCore
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
+        public FlatWhite(int rabat)
+            :base(rabat)
+        {
+        }
+        
+
+
+    }
+}
--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -9,7 +10,39 @@ namespace KaffeConsoleAppNetCore
         {
             Console.WriteLine("Hello World!");
 
-            
+
+            Cortado cortado = new Cortado();
+            Latte latte = new Latte();
+
+            FlatWhite flatwhite = new FlatWhite(12);
+
+
+            List<Imælk> liste = new List<Imælk>();
+            liste.Add(cortado);
+            liste.Add(latte);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine("Før cast " + item.MlMælk());
+
+                if (item is Latte)
+                {
+                    Latte l = item as Latte;
+                    Console.WriteLine("Latte styrke" + l.Styrke());
+                }
+                else if (item is Cortado)
+                {
+                    Cortado c = item as Cortado;
+                }
+            }
+     
+
+
+            Console.ReadLine();
+
+
+
+
 
            
 
